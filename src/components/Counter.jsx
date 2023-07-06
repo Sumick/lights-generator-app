c2f985084d24b0b5b3826b0b677194468880e0d3
Jakub Wąsowski <jakub@nerdz.pl>
2023-07-06 15:01:21 +0200

    feat: generate grid dynamically

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,27 +6,14 @@ import { useState } from "react";
 import styles from "./Counter.module.css";
 
 export const Counter = (props) => {
-  const [value, setValue] = useState(0);
-
-  const handleIncrement = () => {
-    setValue(value + 1);
-  };
-
-  const handleDecrement = () => {
-    if (value <= 0) {
-      return;
-    }
-    setValue(value - 1);
-  };
-
   return (
     <div className={styles.wrapper}>
       <Text>
-        {value} {props.text}
+        {props.count} {props.text}
       </Text>
       <nav>
-        <IconDecrement onClick={handleDecrement} />
-        <IconIncrement onClick={handleIncrement} />
+        <IconDecrement onClick={props.onDecrement} />
+        <IconIncrement onClick={props.onIncrement} />
       </nav>
     </div>
   );

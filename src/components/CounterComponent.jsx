3748ce6e8d9ab892480dba3319d719a5c992674c
Jakub Wąsowski <jakub@nerdz.pl>
2023-07-07 14:31:02 +0200

    feat: add counter

---
+++ src/components/CounterComponent.jsx
@@ -0,0 +1,20 @@
+import { Text } from "./Text";
+import { IconDecrement } from "./icons/IconDecrement";
+import { IconIncrement } from "./icons/IconIncrement";
+import { useState } from "react";
+
+import styles from "./Counter.module.css";
+
+export const Counter = (props) => {
+  return (
+    <div className={styles.wrapper}>
+      <Text>
+        {props.count} {props.text}
+      </Text>
+      <nav>
+        <IconDecrement onClick={props.onDecrement} />
+        <IconIncrement onClick={props.onIncrement} />
+      </nav>
+    </div>
+  );
+};
